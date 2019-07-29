ac046cdbc740fa5b7c533f697934ddeb70f608b0
Cnix24 <calvinattl24@gmail.com>
2019-07-29 14:57:02 -0600

    Update

---
+++ Step165TryCatchExceptions/AgeException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Step165TryCatchExceptions
+{
+    public class AgeException : Exception
+    {
+        public AgeException()
+     : base() { }
+        public AgeException(string message)
+            : base(message) { }
+    }
+}
---
+++ Step165TryCatchExceptions/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step165TryCatchExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                bool validAnswer = false;
+                int age = 0;
+                while (!validAnswer)
+                {
+                    Console.WriteLine("How old are you?");
+                    validAnswer = int.TryParse(Console.ReadLine(), out age);
+                    if (!validAnswer)
+                    {
+                        Console.WriteLine("Please enter your age using only numbers.");
+                        validAnswer = int.TryParse(Console.ReadLine(), out age);
+                    }
+                }
+                if (age < 1)
+                {
+                    throw new AgeException();
+                }
+                int nowYear = DateTime.Now.Year;
+                int userYearBorn = nowYear - age;
+
+                Console.WriteLine("You were born in the year: " + userYearBorn);
+                Console.ReadLine();
+            }
+            catch (AgeException)
+            {
+                Console.WriteLine("You've listed an age less than a year old, how are you even using a computer?");
+                Console.ReadLine();
+                return;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("An error occurred. Please contact your System Admin.");
+                Console.ReadLine();
+                return;
+            }
+        }
+    }
+}
